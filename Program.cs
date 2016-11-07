b80fbb9e9495343acd36753e12005d16b577b7fa
PetterC <petterchan89@gmail.com>
2016-11-07 06:03:40 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StatList<string> player = new StatList<string>();
+            player.Add("Lebron James");
+            player.Add("Russell Westbrook");
+            player.Add("Kevin Durant");
+            player.Add("James Harden");
+            player.Add("Stephen Curry");
+            player.Remove("Anthony Davis");
+            
+
+            StatList<int> ponits = new StatList<int>();
+            ponits.Add("22");
+            ponits.Add("34");
+            ponits.Add("30");
+            ponits.Add("32");
+            ponits.Add("25");
+            ponits.Remove("30");
+           
+
+            StatList<int> rebounds = new StatList<int>();
+            rebounds.Add("10");
+            rebounds.Add("10");
+            rebounds.Add("9");
+            rebounds.Add("7");
+            rebounds.Add("2");
+            rebounds.Remove("12");
+
+            List<string> playerList = new List<string> { "Lebron James", "Russell Wesbrook", "Kevin Durrant", "James Harden", "Stephen Curry", "Anthony Davis" };
+            List<string> ponitsAverage = new List<string> { "22", "34", "30", "32", "25", "30" };
+            List<string> reboundsAverage = new List<string> { "10", "10", "9", "7", "2", "12" };
+
+            var result = CombineListsByLayers(playerList, ponitsAverage, reboundsAverage);
+        }
+
+        public static List<string>[] CombineListsByLayers(params List<string>[] sourceLists)
+        {
+            var results = new List<string>[sourceLists[0].Count];
+
+            for (var i = 0; i < results.Length; i++)
+            {
+                results[i] = new List<string>();
+                foreach (var sourceList in sourceLists)
+                    results[i].Add(sourceList[i]);
+            }
+            return results;
+        }
+
+
+    }
+    }
+
---
+++ StatList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class StatList<T> : IEnumerable
+
+    {
+        string player;
+        int ponits;
+        int rebounds;
+        List<string> topPlayers;
+        List<int> playerPonits;
+        List<int> playerRebounds;
+        
+
+        public StatList()
+        {
+            topPlayers = new List<string>() { "Lebron James", "Russell Westbrook", "Kevin Durant", "James Harden", "Stephen Curry", "Anthony Davis" };
+            playerPonits = new List<int>() { 22, 34, 30, 32, 25, 30 };
+            playerRebounds = new List<int>() { 10, 10, 9, 7, 2, 12 };
+          
+
+        }
+
+        public static StatList<T> operator + (StatList<T> statlist_1, StatList<T> statlist_2)
+        {
+            StatList<T> stat = new StatList<T>();
+
+            stat.player = statlist_1.player + statlist_2.player;
+            stat.ponits = statlist_1.ponits + statlist_2.ponits;
+            stat.rebounds = statlist_1.rebounds + statlist_2.rebounds;
+          
+            return stat;
+        }
+        public static StatList<T> operator - (StatList<T> statlist_1, StatList<T> statlist_2)
+        {
+            StatList<T> stat = new StatList<T>();
+
+            stat.player = statlist_1.player + statlist_2.player;
+            stat.ponits = statlist_1.ponits + statlist_2.ponits;
+            stat.rebounds = statlist_1.rebounds + statlist_1.rebounds;
+
+            return stat;
+
+        }
+
+        public void PlayerStats()
+        {
+            Console.WriteLine(player + "," + ponits + "," + rebounds);
+            Console.ReadLine();
+        }
+
+
+        public void Add(string name)
+        {
+          
+           
+        }
+        public void Remove(string name)
+        {
+
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for(int i = 0; i <6; i++)
+            {
+               yield return i;
+            }
+        
+        }
+    }
+}
